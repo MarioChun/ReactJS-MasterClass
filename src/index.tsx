bb0fbb7cae3fc2d243fb3e6e2f2361d224cfa5b4
Mario <mario@Marioui-iMac.local>
2022-12-05 22:36:08 +0900

    finished #3 Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
